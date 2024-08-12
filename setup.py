fc2ddc60ea49bd90bd2f2a07feddffcedd286a7d
esitarski <edward.sitarski@gmail.com>
2024-08-12 07:57:09 -0400

    Updated to include markdown readme file.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     include_package_data=True,
     description='pyllrp: a pure Python implementation of LLRP (Low Level Reader Protocol).',
     install_requires=['bitstring >= 3.1.1',],
-    classifiers=['Development Status :: 3 - Alpha', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3', 'Operating System :: OS Independent'],
-    long_description=open('README.txt').read(),
+    classifiers=['Development Status :: 5 - Production/Stable', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3', 'Operating System :: OS Independent'],
+    long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',   
 )
